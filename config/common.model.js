c606bd1a14c7d06414fd08dbc58c2377cdc2487b
Bastien NICOLAS <bnicolas.pro@gmail.com>
2021-10-19 10:15:01 +0200

    common config model

--- config/common.model.js
+++ config/common.model.js
@@ -5,12 +5,16 @@ module.exports = {
     env_config: {},
     authentication_credentials: {
         mongodb_uri: 'mongodb://localhost:27017/starter',
-        jwt_secret: 'azer1234',
+        jwt_secret: 'un super secret',
     },
-    settings: {},
+    settings: {
+        user_auth_jwt_lifetime: 3600, // = 1 heure
+        user_refresh_jwt_lifetime: 15778800, // = 6 mois
+        tasks_status: ['Todo', 'Doing', 'Done'],
+    },
     logs: {
         console: false,
-        file: true,
+        file: false,
         debug: true,
     },
     services: {
